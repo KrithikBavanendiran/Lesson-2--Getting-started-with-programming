3121342a17e28d11f54824eaeb4c8ac0105e0c80
KrithikBavanendiran <bvino1522@gmail.com>
2025-02-06 21:40:46 +0000

    Homework- Birthday Variables

---
+++ Birthday variable(Homework).py
@@ -0,0 +1,12 @@
+print("Family member's birthdays")
+krithik="24 January"
+thiyara="14 May"
+tara= "01 October"
+mum= "15 July"
+dad= "22 August"
+
+print("Krithik's birthday:",krithik)
+print("Thiyara's birthday:",thiyara)
+print("Tara's birthday:",tara)
+print("Mum's birthday:",mum)
+print("Dad's birthday:",dad)
